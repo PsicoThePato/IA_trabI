4f668fef2c965cb788e25afaa647f89b6df0cb12
Jairoson <tachatoja@gmail.com>
2021-03-23 02:27:45 -0300

    :poop: :working: sinto que meu pc vai explodir

---
+++ nome_massa/metaAga/generalFuncs.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+
+import numpy as np
+import pandas as pd
+
+
+def random_state(dataset: pd.DataFrame, k: int) -> pd.DataFrame:
+    grupos = np.append(
+        np.random.randint(0, k, size=len(dataset) - k),
+        range(k)
+        )
+    np.random.shuffle(grupos)
+    dataset['C-grupo'] = grupos
+    return dataset
+
+
+def sse_estado(listGroups: List[Tuple[int, pd.DataFrame]]):
+    sse = 0
+    for _, df in listGroups:
+        centroid = (df.iloc[:, 0:-1].sum(axis=0))/len(df)
+        sse += ((df.iloc[:, 0:-1] - centroid) ** 2).sum(axis=1).sum(axis=0)
+        df['centroide'] = centroid
+    print(sse)
+    return sse
---
+++ nome_massa/ponto.py
@@ -0,0 +1,49 @@
+import random
+
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+
+from metaAga import generalFuncs
+import metaAga.genetic
+
+
+class Ponto:
+    def __init__(self, components, identif):
+        self.components = components
+        self.identif = identif
+
+    def __repr__(self):
+        return f"<id: {self.identif}, componentes: {self.components}>"
+
+
+def calcula_euclidiana(linha: pd.Series, centroid: np.array):
+    print(linha)
+    print(centroid)
+    return (linha - centroid)**2
+
+
+populacao = 10
+k = 5
+iris = load_iris()
+iris_df = (pd.DataFrame(data=iris.data, columns=iris.feature_names))
+print(iris_df.head())
+
+estados = []
+sse_list = np.array([])
+sse_total = 0
+for _ in range(populacao):
+    state = generalFuncs.random_state(iris_df, k)
+    estados.append(state.copy())
+    
+    list_by_group = [*state.groupby('C-grupo')]
+    sse = generalFuncs.sse_estado(list_by_group)
+    sse_total += sse
+    sse_list = np.append(sse_list, sse)
+
+sse_list = (1 - sse_list/sse_total)/(populacao - 1)
+choice = np.random.choice(range(populacao), 2, p=sse_list, replace=False)
+pai1 = estados[choice[0]]
+pai2 = estados[choice[1]]
+pai1[145:] = pai2[145:]
+
