cf23a8215d67199400ca796b80ca00abe60995af
Jairoson <tachatoja@gmail.com>
2021-03-26 01:33:14 -0300

    :sparkles: Depois de 3 dias descobri que o unico motivo pelo qual a SA estava :poop: era pq eu estava passando uma variável errada  :clown_face:.

--- nome_massa/metaAga/generalFuncs.py
+++ nome_massa/metaAga/generalFuncs.py
@@ -17,10 +17,8 @@ def random_state(dataset: pd.DataFrame, k: int) -> pd.DataFrame:
 def sse_estado(listGroups: List[Tuple[int, pd.DataFrame]]):
     sse = 0
     for _, df in listGroups:
-        #print(df)
         centroid = (df.iloc[:, 0:-1].sum(axis=0))/len(df)
         sse += ((df.iloc[:, 0:-1] - centroid) ** 2).sum(axis=1).sum(axis=0)
-        #df['centroide'] = centroid
     return sse
 
 
@@ -29,3 +27,38 @@ def append_sse(sse_total, estado, sse_list_return):
     sse = sse_estado(list_by_group)
     sse_total += sse
     return sse_total, np.append(sse_list_return, sse)
+
+
+def calcula_pior_ponto(grupo: pd.DataFrame):
+    centroid = (grupo.iloc[:, 0:-1].sum(axis=0))/len(grupo)
+    dist_pontos = ((grupo.iloc[:, 0:-1] - centroid) ** 2).sum(axis=1)
+    pior_ponto = dist_pontos.idxmax()
+    return pior_ponto
+
+
+def pior_grupo(listGroups):
+    #fazer no calcula sse
+    pior_sse = 0
+    pior_grp = -1
+    for _, df in listGroups:
+        centroid = (df.iloc[:, 0:-1].sum(axis=0))/len(df)
+        sse = ((df.iloc[:, 0:-1] - centroid) ** 2).sum(axis=1).sum(axis=0)
+        if(sse > pior_sse):
+            pior_sse = sse
+            pior_grp = _
+    return pior_grp
+
+
+if __name__ == "__main__":
+    from sklearn.datasets import load_iris
+    iris = load_iris()
+    iris_df = (pd.DataFrame(data=iris.data, columns=iris.feature_names))
+    estado = random_state(iris_df, 5)
+    pior_grp = pior_grupo([*estado.copy().groupby('C-grupo')])
+    print(pior_grp)
+    #print(estado.loc[estado['C-grupo'] == pior_grp])
+    #breakpoint()
+    pior_ponto = calcula_pior_ponto(estado.loc[estado['C-grupo'] == pior_grp])
+    print(estado)
+    breakpoint()
+    print(estado[pior_ponto])
--- nome_massa/metaAga/simulated_annealing.py
+++ nome_massa/metaAga/simulated_annealing.py
@@ -1,5 +1,6 @@
 from math import exp
 import random
+from operator import itemgetter
 
 import pandas as pd
 import numpy as np
@@ -8,24 +9,50 @@ from sklearn.datasets import load_iris
 import generalFuncs
 
 
-def step(estado_atual, temp):
+def step(estado_atual, temp, k):
     factor = exp(- len(estado_atual)/temp)
-    m_lines = np.random.choice(
-        estado_atual.index,
-        (int(len(estado_atual) * factor) + 60),
-        replace=True
-    )
-    teste = estado_atual.iloc[m_lines]['C-grupo']
-    np.random.shuffle(teste.values)
-    estado_atual.loc[m_lines, "C-grupo"] = teste
-    return estado_atual
+    kuga = []
+    sse_list = np.array([])
+    sse_total = 0
+    # for _ in range(10):
+    potential_state = estado_atual.copy()
+        # m_lines = np.random.choice(
+        #     potential_state.index,
+        #     (int(len(potential_state) * factor) + 1),
+        #     replace=False
+        # )
+        # teste = (potential_state.iloc[m_lines]['C-grupo']).copy()
+        # np.random.shuffle(teste.values)
+    pior_grp = generalFuncs.pior_grupo([*potential_state.copy().groupby('C-grupo')])
+    pior_ponto = generalFuncs.calcula_pior_ponto(potential_state.loc[potential_state['C-grupo'] == pior_grp])
+    ponto = np.random.choice(range(k))
+    #print(potential_state.loc[pior_ponto, 'C-grupo'])
+    #print(len(potential_state.loc[potential_state['C-grupo'] == pior_grp]))
+    potential_state.at[pior_ponto, 'C-grupo'] = ponto
+    #print(potential_state.loc[pior_ponto, 'C-grupo'])
+    #print(len(potential_state.loc[potential_state['C-grupo'] == pior_grp]))
+    #breakpoint()
+    return potential_state
+
+
+
+        # for idx, _ in enumerate(teste):
+        #     teste[idx] = np.random.choice(range(k))
+        #potential_state.loc[m_lines, "C-grupo"] = teste
+    #     sse_total, sse_list = generalFuncs.append_sse(sse_total, potential_state.copy(), sse_list)
+    #     kuga.append(potential_state)
+
+    # sse_list = (1 - sse_list/sse_total)/(len(sse_list) - 1)
+    # idx = np.random.choice(range(len(sse_list)), p=sse_list)
+    # best_state = kuga[idx]
+    # return best_state
 
 
 if __name__ == '__main__':
     k = 10
     nIter = 350
     temp = 500
-    a = 0.95
+    a = 0.7
     iris = load_iris()
     iris_df = (pd.DataFrame(data=iris.data, columns=iris.feature_names))
     state = generalFuncs.random_state(iris_df, k)
@@ -33,15 +60,18 @@ if __name__ == '__main__':
     best_estado = (state, sse)
     estado_comparacao = state
     sse_comparacao = sse
+    for i in range(5):
+        print("Temp desceu")
+        for _ in range(nIter):
+            state_candidato = step(estado_comparacao, temp, k)
+            sse_candidato = generalFuncs.sse_estado(*[state_candidato.copy().groupby('C-grupo')])
 
-    for _ in range(nIter):
-        state_candidato = step(state, temp)
-        sse_candidato = generalFuncs.sse_estado(*[state_candidato.copy().groupby('C-grupo')])
-        if(sse_candidato < best_estado[1]):
-            best_estado = (state_candidato, sse_candidato)
-            print(f"Estamos na iteracao {_} e o melhor estado tem sse {sse_candidato}")
-        delta = sse_candidato - sse_comparacao
+            delta = sse_candidato - sse_comparacao
+            if(delta < 0 or random.uniform(0, 1) < exp(-delta/(temp+1))):
+                #print("aceitei")
+                estado_comparacao, sse_comparacao = state_candidato, sse_candidato
 
-        if(delta < 0 or random.uniform(0, 1) < exp(-delta/(temp+1))):
-            estado_comparacao, sse_comparacao = state_candidato, sse_candidato
-            temp = temp * a
+            if(sse_candidato < best_estado[1]):
+                best_estado = (state_candidato, sse_candidato)
+                print(f"Estamos na iteracao {_} e o melhor estado tem sse {sse_candidato}")
+        temp = temp * a
